98e35ce7fe8e20058e033fd435fde8870086fead
Erika Lopez <lopez1erika@gmail.com>
2014-11-05 22:17:00 -0300

    generate comment scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106002354) do
+ActiveRecord::Schema.define(version: 20141106011640) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.string   "author"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
